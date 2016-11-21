90a702ffddfe2bc19a67810b95f7e71ffda6d8fb
kezarberger <kezarberger@hanovernorwichschools.org>
2016-11-21 13:31:56 -0500

    Updated from Brython Server: 11/21/2016 1:31:56 PM

--- cryptography.py
+++ cryptography.py
@@ -11,8 +11,10 @@ See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptog
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 no = input("Enter e to encrypt, d to decrypt, or q to quit: ")
-
-if no == ("q"):
-    print ("Goodbye!")
-elif no not in ("e", "d", "q"):
-     input("Did not understand command, try again:")
+again = True
+while again == True:
+    if no == ("q"):
+        print ("Goodbye!")
+        again = False
+    elif no not in ("e", "d", "q"):
+        no = input("Did not understand command, try again:")
