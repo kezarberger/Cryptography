8411286c12e7817610730500a991d77a5cab3c00
kezarberger <kezarberger@hanovernorwichschools.org>
2017-01-11 11:10:40 -0500

    Updated from Brython Server: 1/11/2017 11:10:40 AM

--- cryptography.py
+++ cryptography.py
@@ -9,6 +9,7 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 again = True
 while again == True:
